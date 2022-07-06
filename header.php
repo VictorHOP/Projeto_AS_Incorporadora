6b0d6024ac57f4ab7e39634b8a50e23adac001cb
VictorHOP <victorhugo.op@outlook.com>
2022-07-06 17:26:53 -0300

    primeiro commit

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Home - AsIncorporadora</title>
+</head>
+
+<body>
+
+    <header class="position-relative">
+
+        <div class="logo">
+            <img src="assets/imgs/logo-AS.png" alt="Logo AS Incorporadora">
+        </div>
+        <div class="d-flex justify-content-between h-100 align-items-center">
+            <nav class="menu h-100 d-flex align-items-center">
+                <a href="">HOME</a>
+                <a href="">EMPREENDIMENTOS</a>
+                <a href="">BLOG</a>
+                <a href="">CONTATO</a>
+            </nav>
+            <div>
+                <button class="botao-preto"><img src="assets/imgs/icone-colaborador.png" alt="Icone 3 pessoas">
+                    COLABORADOR</button>
+                <button class="botao-vermelho"><img src="assets/imgs/icone-cliente.png" alt="icone usuario">
+                    CLIENTE</button>
+            </div>
+        </div>
+    </header>
