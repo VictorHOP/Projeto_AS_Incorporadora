29f7db4b17f19fa5b5b196ba1846dbc93fa9414f
VictorHOP <victorhugo.op@outlook.com>
2022-07-13 17:57:59 -0300

    pagina post blog e erro 404 finalizadas falta só arrumar menu, responsivo dos swipers

--- header.php
+++ header.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap.css">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="style.css">
 
     <title>Home - AsIncorporadora</title>
 </head>
@@ -17,7 +17,7 @@
     <header class="position-relative">
 
         <div class="logo">
-            <img src="assets/imgs/logo-AS.png" alt="Logo AS Incorporadora">
+            <img src="assets/images/logo-AS.png" alt="Logo AS Incorporadora">
         </div>
         <div class="d-flex justify-content-between h-100 align-items-center">
             <nav class="menu h-100 d-flex align-items-center">
@@ -27,9 +27,9 @@
                 <a href="">CONTATO</a>
             </nav>
             <div>
-                <button class="botao-preto"><img src="assets/imgs/icone-colaborador.png" alt="Icone 3 pessoas">
+                <button class="botao-preto"><img src="assets/images/icone-colaborador.png" alt="Icone 3 pessoas">
                     COLABORADOR</button>
-                <button class="botao-vermelho"><img src="assets/imgs/icone-cliente.png" alt="icone usuario">
+                <button class="botao-vermelho"><img src="assets/images/icone-cliente.png" alt="icone usuario">
                     CLIENTE</button>
             </div>
         </div>
